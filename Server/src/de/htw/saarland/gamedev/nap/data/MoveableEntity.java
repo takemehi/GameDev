4ba904ad1a7d54ba376fe86db4b43e9933fc0508
Stefan <sbearzatto@gmx.de>
2013-12-12 17:46:19 +0100

    entity änderungen

--- Server/src/de/htw/saarland/gamedev/nap/data/MoveableEntity.java
+++ Server/src/de/htw/saarland/gamedev/nap/data/MoveableEntity.java
@@ -1,31 +1,83 @@
 package de.htw.saarland.gamedev.nap.data;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Shape;
 
 public class MoveableEntity extends Entity{
 
 	//base values
-	private Vector2 baseAcceleration;
-	private Vector2 baseVelocity;
-	private int weight;
+	private Shape shape;
+	private float weight;
+	private float density;
+	private float friction;
+	private float restitution;
 	
 	//momentary values
 	private Vector2 acceleration;
 	private Vector2 velocity;
 	private Vector2 position;
 	
-	public MoveableEntity(){
-		
-	}
+	//internal values
+	FixtureDef fixtureDef;
+	BodyDef bodyDef;
 	
-	public MoveableEntity(Vector2 position){
+	//////////////////////
+	//	constructors	//
+	//////////////////////
+	
+	public MoveableEntity(Shape shape, float weight, float density,
+			float friction, float restitution, Vector2 position) {
+		super();
+		this.shape = shape;
 		this.weight = weight;
+		this.density = density;
+		this.friction = friction;
+		this.restitution = restitution;
+		this.position = position;
+		
+		initBodyDef();
+		initFixtureDef();
+	}
+
+	//////////////////////
+	//	intern methods	//
+	//////////////////////
+	
+	//initialization methods
+
+	private void initBodyDef(){
+		bodyDef=new BodyDef();
+		bodyDef.type=BodyDef.BodyType.DynamicBody;
+		bodyDef.position.set(position);
 	}
 	
-	public MoveableEntity(Vector2 position, int weight, Vector2 baseAcceleration, Vector2 baseVelocity){
-		this(position);
-		this.weight=weight;
-		this.baseAcceleration=baseAcceleration;
-		this.baseVelocity=baseVelocity;
+	private void initFixtureDef(){
+		fixtureDef = new FixtureDef();
+		fixtureDef.density=density;
+		fixtureDef.friction=friction;
+		fixtureDef.restitution=restitution;
+	}
+	
+	//////////////////////
+	//	public methods	//
+	//////////////////////
+	
+	public void dispose(){
+		shape.dispose();
+	}
+	
+	//////////////////////
+	//	getter / setter	//
+	//////////////////////
+	
+	public FixtureDef getFixtureDef() {
+		return fixtureDef;
+	}
+
+	public BodyDef getBodyDef() {
+		return bodyDef;
 	}
 }
--- Server/src/de/htw/saarland/gamedev/nap/game/GameServer.java
+++ Server/src/de/htw/saarland/gamedev/nap/game/GameServer.java
@@ -1,13 +1,119 @@
 package de.htw.saarland.gamedev.nap.game;
 
-import com.badlogic.gdx.Game;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class GameServer extends Game {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.smartfoxserver.v2.entities.data.SFSObject;
 
+
+public class GameServer implements ApplicationListener {
+	
+	private final static int PACKETS_PER_TICK = 50;
+
+	private String map;
+	private int teamSize;
+	private ConcurrentLinkedQueue<SFSObject> packetQueue;
+	
+	private World world;
+	//TODO remove debugrenderer and camera
+	private Box2DDebugRenderer renderer;
+	private Vector2 gravity;
+	private OrthographicCamera camera;
+	
+	
+	//TODO Map
+	public GameServer(String map, int teamSize){
+		this.map=map;
+		this.teamSize=teamSize;
+		
+		//////////////////////////
+		//	box2d stuff			//
+		//////////////////////////
+		gravity = new Vector2(0,-9.81f);
+		world = new World(gravity, true);
+		renderer = new Box2DDebugRenderer();
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		//instantiate variables
+		//load stuff from external files
+		//load chosen Map	
+	}
+	
+	//////////////////////////
+	//	override methods	//
+	//////////////////////////
+	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//TODO AI, capture points
+	
+	@Override
+	public void render() {
+		//get client packages
+		for(int i=0; i<PACKETS_PER_TICK; i++){
+			packetQueue.remove();
+			//do damn cool stuff
+			//nigga
+		}
+		
+		//recalculate positions and
+		//check for dem collisions
+		
+		//check for ability collisions, damage abilities
+		
+		//calculate hp, apply status effects, recalculate position
+		
+		//send dat shit
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		renderer.render(world, camera.combined);
+	}
+
+	@Override
+	public void pause() {
+		//pausing is for pussies
+		//thus not supported!
+		
+	}
+
+	@Override
+	public void resume() {
+		//see above
+	}
+
+	@Override
+	public void dispose() {
+		world.dispose();
+		renderer.dispose();		
+	}
+	
+	//////////////////////
+	//	own methods		//
+	//////////////////////
+
+	/**
+	 * This method accepts incoming packets from players
+	 * 
+	 * @param packet Packet with recent player action informations
+	 */
+	public void addPacket(SFSObject packet){
+		if(packet == null) throw new NullPointerException();
+		packetQueue.add(packet);
+	}
 }
